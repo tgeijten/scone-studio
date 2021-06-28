5817cf231f9d4acd2fc61f4406d3d6bf1a2010f6
Thomas Geijtenbeek <thomas@goatstream.com>
2021-06-28 17:07:13 +0200

    U: Improved Help -> About... and other help menu items S: scone

--- src/sconestudio/help_tools.h
+++ src/sconestudio/help_tools.h
@@ -6,4 +6,7 @@
 namespace scone
 {
 	QUrl GetHelpUrl( const QString& keyword );
+	inline QUrl GetWebsiteUrl() { return QUrl( "https://scone.software" ); }
+	inline QUrl GetDownloadUrl() { return QUrl( "https://simtk.org/frs/?group_id=1180" ); }
+	inline QUrl GetForumUrl() { return QUrl( "https://simtk.org/plugins/phpBB/indexPhpbb.php?group_id=1180&pluginname=phpBB" ); }
 }
