64f244923389ec6063bda5e2f5ecfd21509948da
Thomas Geijtenbeek <thomas@goatstream.com>
2021-07-12 18:08:52 +0200

    U: tryInstallTutorials()

--- src/sconestudio/file_tools.cpp
+++ src/sconestudio/file_tools.cpp
@@ -3,6 +3,11 @@
 #include "QDirIterator"
 #include "scone/core/system_tools.h"
 #include "xo/system/log.h"
+#include <filesystem>
+#include "QMessageBox"
+#include "xo/filesystem/filesystem.h"
+
+namespace fs = std::filesystem;
 
 namespace scone
 {
@@ -35,8 +40,23 @@ namespace scone
 
 	void tryInstallTutorials()
 	{
-		auto sourceDir = scone::GetFolder( scone::SCONE_ROOT_FOLDER );
-		auto targetDir = scone::GetFolder( scone::SCONE_SCENARIO_FOLDER );
-		xo::log::info( "source=", sourceDir, " target=", targetDir );
+		auto sconeDir = scone::GetFolder( scone::SCONE_SCENARIO_FOLDER );
+		auto tutDir = sconeDir / "Tutorials";
+		auto instDir = scone::GetFolder( scone::SCONE_ROOT_FOLDER ) / "scenarios";
+		auto fsScone = fs::path( sconeDir.str() );
+		auto fsInst = fs::path( instDir.str() );
+		if ( fs::exists( fsScone / "Tutorials/Tutorial 1 - Introduction.scone" ) )
+		{
+			auto tutBackupDir = tutDir + "Backup";
+			QString msg = QString( "Existing tutorials will be copied to\n\n" ) + tutBackupDir.c_str();
+			if ( QMessageBox::question( nullptr, "Rename Tutorials", msg ) != QMessageBox::Yes )
+				return;
+			auto sconeTutBackup = fs::path( xo::find_unique_directory( tutDir + "Backup" ).str() );
+			fs::rename( fsScone / "Tutorials", sconeTutBackup );
+		}
+
+		auto options = fs::copy_options::update_existing | fs::copy_options::recursive;
+		fs::copy( fsInst / "Tutorials", fsScone / "Tutorials", options );
+		fs::copy( fsInst / "Examples", fsScone / "Examples", options );
 	}
 }
