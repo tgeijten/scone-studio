14eb37f5c4d5cf8df4a727d245ed4af612b940ea
Thomas Geijtenbeek <thomas@goatstream.com>
2023-02-20 23:15:46 +0100

    B: only create .deb package and no .tar.gz

--- tools/linux_create-scone-studio-package.sh
+++ tools/linux_create-scone-studio-package.sh
@@ -9,14 +9,12 @@ source "${script_dir}/build_config"
 
 export SCONE_CREATE_DEBIAN_PACKAGE=ON
 ${script_dir}/unix_2d_build-scone-studio-hfd
-
 cd "${SCONE_BUILD_DIR}"
 cpack
 cd ..
   
-export SCONE_CREATE_DEBIAN_PACKAGE=OFF
-${script_dir}/unix_2d_build-scone-studio-hfd
-
-cd "${SCONE_BUILD_DIR}"
-cpack
-cd ..
+# export SCONE_CREATE_DEBIAN_PACKAGE=OFF
+# ${script_dir}/unix_2d_build-scone-studio-hfd
+# cd "${SCONE_BUILD_DIR}"
+# cpack
+# cd ..
