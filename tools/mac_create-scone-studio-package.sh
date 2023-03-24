769c2ae6a70d1dfdbe9d6d8030b9214f74cdb633
Thomas Geijtenbeek <thomas@goatstream.com>
2023-03-24 15:34:57 +0100

    B: macOS build updates, tools script cleanup

--- tools/mac_create-scone-studio-package.sh
+++ tools/mac_create-scone-studio-package.sh
@@ -5,4 +5,4 @@ source "$( dirname ${BASH_SOURCE} )/build_config"
 
 ${SCONE_REPOSITORY_DIR}/tools/unix_2d_build-scone-studio-hfd
 ${SCONE_REPOSITORY_DIR}/tools/mac_3_create-install-dirtree-fix
-${SCONE_REPOSITORY_DIR}/tools/mac_4_create-dmg
+${SCONE_REPOSITORY_DIR}/tools/mac_4_create-dmg-fix
